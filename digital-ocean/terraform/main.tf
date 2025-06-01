40e43b3df625ae9ecd09687775610a895a86aba5
Philip Mello <110171839+PhilipMello@users.noreply.github.com>
2025-06-01 22:37:57 +0100

    💫Update (main.tf

--- digital-ocean/terraform/main.tf
+++ digital-ocean/terraform/main.tf
@@ -8,15 +8,21 @@ terraform {
 }
 
 provider "digitalocean" {
-  token = "your_digitalocean_token_here"
+  token = "YOUR_DIGITALOCEAN_TOKEN_HERE"
 }
 
 # Create a DigitalOcean Project
 resource "digitalocean_project" "k8s_from_scratch" {
-  name        = "k8s-from-scratch"    # Name of the project
+  name        = "k8s-from-scratch"          # Name of the project
   description = "Kubernetes from Scratch"   # Optional description
-  purpose     = "Studying"    # Purpose of the project, such as "Web App", "Database", etc.
+  purpose     = "Studying"                  # Purpose of the project, such as "Web App", "Database", etc.
   environment = "Development"
+  resources = [
+    digitalocean_droplet.k8s_from_scratch_control_plane.urn, 
+    digitalocean_droplet.k8s_from_scratch_worker_node_1.urn, 
+    digitalocean_droplet.k8s_from_scratch_worker_node_2.urn
+    ]
+
 }
 
 
