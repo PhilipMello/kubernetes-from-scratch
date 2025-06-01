e786ce2db0fe3875805b81ae9f88a55ab7c6ab0c
Philip Mello <110171839+PhilipMello@users.noreply.github.com>
2025-06-01 10:48:43 +0100

    💫Update (vars.tf)

--- digital-ocean/terraform/vars.tf
+++ digital-ocean/terraform/vars.tf
@@ -1,14 +1,23 @@
 variable "region" {
   description = "Location: London (LON1)"
-  region = "lon1"
+  type = string
+  default = "lon1"
 }
 
 variable "size" {
   description = "2 vCPUs, 2GB RAM"
-  size = "s-2vcpu-2gb"
+  type = string
+  default = "s-2vcpu-2gb"
 }
 
 variable "image" {
   description = "You can change this to any available image (e.g., Ubuntu, CentOS)"
-  image  = "ubuntu-24-04-x64"
+  type = string
+  default = "ubuntu-24-04-x64"
+}
+
+variable ssh_keys {
+  description = "Replace with your actual SSH key ID or list of IDs"
+  type = string
+  default = "1234567890"
 }
